744070c0c83f77f0512f607549636b36e10aec7b
Mohammad Ali <68131927+Ali19739953@users.noreply.github.com>
2023-08-30 00:19:04 +0400

    Part 1 Challenge#1

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-if (bmiJohn > bmiMark) {
-    console.log("jhon's BMI is higher");
-}
-else {
-    console.log("mark's bmi is higher");
-}
-console.log(`john's bmi is ${+ bmiJohn} and mark's bmi is  ${bmiMark}`);
+//data 1
+const massMark = 78;
+const markHeight = 1.69
+const massJohn = 92;
+const johnHeight = 1.95
+const bmiJohn = massJohn / johnHeight ** 2;
+const bmiMark = massMark / markHeight ** 2;
+console.log(bmiJohn);
+console.log(bmiMark);

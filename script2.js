6239579b66f4a58dcdbeacca3b6a7f81351ac44d
Mohammad Ali <68131927+Ali19739953@users.noreply.github.com>
2023-08-30 00:20:17 +0400

    Part 1 Challenge#2

---
+++ script2.js
@@ -0,0 +1,7 @@
+if (bmiJohn > bmiMark) {
+    console.log("jhon's BMI is higher");
+}
+else {
+    console.log("mark's bmi is higher");
+}
+console.log(`john's bmi is ${+ bmiJohn} and mark's bmi is  ${bmiMark}`);

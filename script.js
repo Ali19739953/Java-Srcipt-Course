d9c6dd58ef78236d5df9349deb0cf6edf2c22fab
Mohammad Ali <68131927+Ali19739953@users.noreply.github.com>
2023-08-30 00:15:46 +0400

    PART 1 Challenge 1
    This is the script file for part 1,challenge 1

---
+++ script.js
@@ -0,0 +1,9 @@
+//data 1
+const massMark = 78;
+const markHeight = 1.69
+const massJohn = 92;
+const johnHeight = 1.95
+const bmiJohn = massJohn / johnHeight ** 2;
+const bmiMark = massMark / markHeight ** 2;
+console.log(bmiJohn);
+console.log(bmiMark);
